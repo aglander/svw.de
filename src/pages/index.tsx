3972c36a8b3a5bb068379d792b1e4457b37bfa3f
Arian Glander <arian.glander@nb-aglander2.fritz.box>
2023-09-10 15:45:37 +0200

    title tag bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,21 +70,21 @@ const IndexPage: React.FC<PageProps> = () => {
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Spiele</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByGames.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.games}</td></tr>)}
+          {playersByGames.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.games}</td></tr>)}
         </tbody></table></figure>
       </div>
 
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Tore</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByGoals.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.goals}</td></tr>)}
+          {playersByGoals.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.goals}</td></tr>)}
         </tbody></table></figure>
       </div>
 
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Vorlagen</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByAssists.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.assists}</td></tr>)}
+          {playersByAssists.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.assists}</td></tr>)}
         </tbody></table></figure>
       </div>
     </div>
@@ -93,21 +93,21 @@ const IndexPage: React.FC<PageProps> = () => {
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Punkte pro Spiel</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByPointsPerGame.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.pointsPerGame.toFixed(1)}</td></tr>)}
+          {playersByPointsPerGame.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.pointsPerGame.toFixed(1)}</td></tr>)}
         </tbody></table></figure>
       </div>
 
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Tore pro Spiel</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByGoalsPerGame.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.teamGoalsPerGame.toFixed(1)}</td></tr>)}
+          {playersByGoalsPerGame.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.teamGoalsPerGame.toFixed(1)}</td></tr>)}
         </tbody></table></figure>
       </div>
 
       <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
         <p><strong>Gegentore pro Spiel</strong></p>
         <figure className="wp-block-table"><table><tbody>
-          {playersByConcededGoalsPerGame.map((player) => <tr><td title="{player.name}">{player.name}</td><td>{player.concededTeamGoalsPerGame.toFixed(1)}</td></tr>)}
+          {playersByConcededGoalsPerGame.map((player) => <tr><td title={player.name}>{player.name}</td><td>{player.concededTeamGoalsPerGame.toFixed(1)}</td></tr>)}
         </tbody></table></figure>
       </div>
     </div>
