6e91952f4853fd23583db5557037da151312a580
aglander <glander@me.com>
2024-10-23 13:13:31 +0000

    externes stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
-import "./src/styles/global.css"
+export const onClientEntry = () => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://www.svwoltersdorf.de/wp-content/themes/twentyfourteen/style.css"; // URL des Stylesheets
+    document.head.appendChild(link);
+  };
